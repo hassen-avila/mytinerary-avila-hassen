eb3c5b05ec4b4e261e72814644a12090c78fd9d5
hassen avila <hassenavilaquintero@gmail.com>
2022-06-06 22:34:15 -0300

    navbar y carrusel

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import info from './data.js'
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+
+
+
+// import required modules
+import { Grid, Pagination } from "swiper";
+
+console.log(info)
+function Main() {
+  return (
+    <div className="App">
+    <>
+      <Swiper
+        slidesPerView={2}
+        grid={{
+          rows: 2,
+        }}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Grid, Pagination]}
+        className="mySwiper"
+      >
+        {info.map(e=> <SwiperSlide><img className='country' src={e.img}/></SwiperSlide>)}
+        </Swiper>
+    </> 
+    </div>
+  );
+}
+
+export default Main;
