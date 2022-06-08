84ab726d1e8f5f2aa172656cf50a0e483e75aabd
hassen avila <hassenavilaquintero@gmail.com>
2022-06-08 14:57:17 -0300

    pagina terminada

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function Title(){
     return(
     <div className='title-box'>
-    <h1 className='title-style' >MYtinerary</h1>
+    <h1 className='title-style' >MyTinerary</h1>
     </div>
     )
 }
