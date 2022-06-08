2c9127f0b1d5850a2aa11b5a1254d965515e8bb2
hassen avila <hassenavilaquintero@gmail.com>
2022-06-08 18:50:06 -0300

    pagina terminada

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../index.css';
 import Slogan from '../components/slogan'
 import ButtonC from '../components/buttonCity'
-import Main from '../components/main'
+import Main from '../components/carousel'
 
 
 export default function Index(){
